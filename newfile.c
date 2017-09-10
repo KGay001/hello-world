61bc84438f40bf3dc295529850138b788b9aa117
Kadesha <Kadesha@Lazena-ASUS>
2017-09-10 18:22:42 -0400

    new file added

---
+++ newfile.c
@@ -0,0 +1,9 @@
+/*sample code taken from Dr. Radix*/ 
+
+#include <p18cxxx.h>
+
+/* Set configuration bits for use with PICKit3 */
+#pragma config FOSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
